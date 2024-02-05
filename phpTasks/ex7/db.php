89326441023e11ddc809e855c34e797de3bf0896
JanikaSalminen <01janikas@gmail.com>
2024-02-05 14:01:59 +0200

    ex7 incomplete

---
+++ phpTasks/ex7/db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "shell.hamk.fi"; // Replace with your MySQL server hostname
+$username = "janika23001";     // Replace with your MySQL username
+$password = "X76f8x1g";     // Replace with your MySQL password
+$dbname = "wp_janika23001";       // Replace with the name of your MySQL database
+
+// Create a database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
